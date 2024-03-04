47cd0135c97359a45a68497ccea673020a1529eb
mjhartley <mike@the-hartleys.co.uk>
2024-03-04 14:46:08 +0000

    Completed exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,15 @@
-const removeFromArray = function(iArray, iRemove) {
-
-
-    // 
-
-    //
-
-
-    iRemove = parseInt(iRemove);
-    iRemove = iRemove - 1;
-    iArray.splice(iRemove, 1);
-
-    return iArray;
-
-};
+const removeFromArray = function (array, ...args) {
+    // create a new empty array
+    const newArray = [];
+        // use forEach to go through the array
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+          }
+        });
+        return newArray;
+      };
+      
 
 // Do not edit below this line
 module.exports = removeFromArray;
