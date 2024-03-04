eaf8ef2c7ca7741872d6a02b55aba3990afc07ab
mjhartley <mike@the-hartleys.co.uk>
2024-03-04 14:47:14 +0000

    Completed exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1 , num2) {
+
+    if ((isNaN(num1)) || (isNaN(num2))){
+         return 'ERROR';
+    }
+
+    if((num1 < 0) || (num2 <0)){
+
+        return 'ERROR';
+    }
+
+
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+
+        return 'ERROR';
+
+    }
+
+
+
+    let result = 0;
+
+    for (let index = num1; index <= num2; index++) {
+        result += index;       
+    }
+
+    return result;
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,33 @@
-const convertToCelsius = function() {
+/* const convertToCelsius = function(temp) {
+
+    temp = parseInt(temp);
+
+    temp  = (temp - 32) * (5/9);
+
+    return temp.toFixed(1);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  temp = parseInt(temp);
+
+  temp  = (temp * (9/5)) + 32;
+
+  return temp.toFixed(1);
+
+
+
+
+}; */
+
+
+const convertToCelsius = function (fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+};
+
+const convertToFahrenheit = function (celsius) {
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
